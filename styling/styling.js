fd384b90a79e9879a5412e3c15c0bdbe221614df
jjivy12 <jordan.ivy12@gmail.com>
2020-04-08 17:01:51 -0400

    styling practice

---
+++ styling/styling.js
@@ -0,0 +1,13 @@
+
+
+/*$(document).ready(function(){
+   $("body").css("background-color: red");
+  });*/
+
+$(document).ready(function(){
+
+$("#h").css("color", "blue")
+$("#bd").css("background-color", "red")
+
+
+});
