27ca0104ca4c4a6648dad964e7eb953ecd55368b
jjivy12 <jordan.ivy12@gmail.com>
2020-04-08 08:57:24 -0400

    Sandwich activity

---
+++ sandwhichCalulator.js
@@ -0,0 +1,17 @@
+function sandwichCalculator(slicesOfBread, slicesOfCheese){
+    var sandwiches = math.floor(slicesOfBread / 2);
+
+    if ((SlicesOfBread > 1) && (slicesOfCheese > 0) (slicesOfCheese < sandwiches)) {
+        sandwiches = slicesOfCheese
+        console.log(sandwiches);
+        return sandwiches;
+    } else if ((slicesOfCheese >= sandwiches) &&(slicesOfBread > 1)) {
+        console.log(sandwiches);
+        return sandwiches;
+    } else if ((slicesOfCheese = 0) || (slicesOfBread , 2)){
+        console.log ("no sandwhiches, my friend.");
+        return 0;
+    }
+
+}
+sandwichCalculator(10, 1);
